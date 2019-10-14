cd0cf58cc650607290abbcb993df86f5715e1ef0
Anton Lydike <me@AntonLydike.de>
2019-10-14 18:28:10 +0200

    added CORS support to backend

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+header("Access-Control-Allow-Headers: X-UID");
+
 require_once 'settings.php';
 require_once 'sql.php';
 require_once 'auth.php';
@@ -16,5 +19,11 @@ foreach (glob("handlers/*.php") as $filename) {
 foreach (glob("methods/*.php") as $filename) {
     require_once $filename;
 }
+
+# handle all preflight requests
+register_method('OPTIONS', '**', function () {
+  http_response_code(STATUS_OK);
+  die();
+});
 
 handle_request();
